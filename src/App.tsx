3e5a73bbdc0f605242a7015a7d8b9b9c4a395843
Sudhanshu Gupta <sudgupt@microsoft.com>
2021-01-22 01:02:53 +0530

    Fixed issues with npm publish

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import SearchBoxDemo from "./AutocompleteSearchBoxExample/SearchBoxDemo";
+import SearchBoxDemo from "./Examples/SearchBoxDemo";
 
 function App() {
   return (
